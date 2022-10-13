4503b58a80192ee0c8a3b523b122db9fe80b4af9
isaacudofia <isaacudofia0@gmail.com>
2022-10-13 22:53:48 +0100

    Initial Commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,15 @@
-const signupUser = (req, res)=>{
+const bcrypt = require('bcrypt')
+const User = require('../model/userModel')
+
+const signupUser = async(req, res)=>{
+    const {email ,password} = req.body
     try {
-       res.status(201).json({msg: 'Signup Successful'})
+       const salt = await bcrypt.genSalt(10)
+       const hashedPassword = await bcrypt.hash(password, salt)
+       const userExist = await User.findOne({email})
+       if(userExist) throw Error("Email already in use")
+       const userCreated = await User.create({email,password: hashedPassword})
+       res.status(201).json({data: "User created in database" })
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,3 +1,12 @@
 const auth =(req, res, next)=>{
-
+  try {
+    const authorization = req.headers['authorization']
+    res.status(200)
+    console.log('auth successful')
+    next()
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
+
+module.exports = auth

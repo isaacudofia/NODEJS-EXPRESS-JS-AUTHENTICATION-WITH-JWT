695fa2344b7bc106c92cfaba9f1ca7de72a111fd
isaacudofia <isaacudofia0@gmail.com>
2022-10-14 21:44:54 +0100

    Bugs Fixed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,16 @@
-const auth =(req, res, next)=>{
+const jwt = require('jsonwebtoken')
+
+const auth =async(req, res, next)=>{
+  const {authorization} = req.headers
+  if(!authorization) return res.status(401).json({error: "Authorization token required"})
+  const token = authorization.split(' ')[1]
   try {
-    const authorization = req.headers['authorization']
-    res.status(200)
-    console.log('auth successful')
+    const {_id} = await jwt.verify(token, process.env.SECRET_TOKEN)
+    req.user = await User.findOne({_id}).select('_id')
+    console.log(req.user);
     next()
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(401).json({error: "Request is not authorized"})
   }
 }
 

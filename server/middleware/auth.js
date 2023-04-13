e2a242ae402174b567efca861ddf1ab7c37cbce3
ISAAC UDOFIA <100437247+isaacudofia@users.noreply.github.com>
2023-04-13 20:34:35 +0100

    New Commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,6 +4,9 @@ const User = require('../model/userModel')
 const auth =async(req, res, next)=>{
   const {authorization} = req.headers
   if(!authorization) return res.status(401).json({error: "Authorization token required"})
+
+  //SPILT ON TOKEN FROM STRING OF CHARACTER
+
   const token = authorization.split(' ')[1]
   try {
     const DecodedToken = jwt.verify(token, process.env.SECRET_TOKEN)

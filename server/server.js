9831aa241106e71d785bdab2206f32d2652a5467
ISAAC UDOFIA <100437247+isaacudofia@users.noreply.github.com>
2025-06-05 18:18:37 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ app.use(express.json())
 app.use('/api/contact', contactRoutes)
 app.use('/api/user', userRoutes)
 
-
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> app.listen( process.env.PORT, ()=> console.log("Connected to DB & listening to port " + process.env.PORT)))
 .catch((err)=> console.log(err.msg))
